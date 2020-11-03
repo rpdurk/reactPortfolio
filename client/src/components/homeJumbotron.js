f8930e32f3d8e8e7f38820b4762dab40aa306746
Ryan Durk <rpdurk@gmail.com>
2020-11-03 14:10:58 -0800

    added images and jumbotron

--- client/src/components/homeJumbotron.js
+++ client/src/components/homeJumbotron.js
@@ -1,10 +1,25 @@
-/* <div class="jumbotron mb-0 d-flex align-items-center flex-column justify-content-center min-100" style="background-image: url(./Images/pgbc.jpg); background-size: cover;" id="header">
-<!-- Start of welcome text(container for opaque text) -->
-    <div class="card welcome" style="width: 20rem;">
-        <div class="card-body">
-        <h1 class="card-title">Hi, my name is Ryan Durk.</h1>
-        <p class="card-text">Explore my portfolio.</p>
-        </div>
-    </div>
-<!-- End welcome text (container for opaque text) -->
-</div> */
+import React from "react";
+import pgbc from "../images/Images/pgbc.png";
+import { MDBJumbotron, MDBContainer, MDBRow, MDBCol, MDBCardTitle } from "mdbreact";
+
+const JumbotronHomePage = () => {
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol>
+          <MDBJumbotron style={{ padding: 0 }}>
+            <MDBCol className="text-white text-center py-5 px-4 my-5" style={{ backgroundImage: `url(${pgbc})` }}>
+              <MDBCol className="py-5">
+                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Hi, my name is Ryan Durk</MDBCardTitle>
+                <p className="mx-5 mb-5">Explore my portfolio.
+                </p>
+              </MDBCol>
+            </MDBCol>
+          </MDBJumbotron>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  )
+}
+
+export default JumbotronHomePage;
