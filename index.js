86a55c5a57acff25956b59249dec2ba5d59ac6d7
Ryan Durk <rpdurk@gmail.com>
2020-11-09 15:27:35 -0800

    updated path for keroku

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-}
+  app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
