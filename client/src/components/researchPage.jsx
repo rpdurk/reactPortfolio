090fc0a23cc16c46dfbd790a82e66c4968336e22
Ryan Durk <rpdurk@gmail.com>
2020-11-08 18:58:26 -0800

    updated research page

--- client/src/components/researchPage.jsx
+++ client/src/components/researchPage.jsx
@@ -1,74 +1,57 @@
 import React from "react";
 import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup';
+import CardColumns from 'react-bootstrap/CardColumns';
+import vo2max from '../Images/vo2max.jpg';
+import wser2019 from '../Images/wser2019.jpg';
+import thaiIslands from '../Images/thaiislands.jpg';
 import { Button } from 'react-bootstrap'; 
 
 
 export const ResearchPage = () => {
   return (
     <>
-    <Card className="text-center" border="light">
-      <Card.Body>
-        <Card.Title>Mission</Card.Title>
-        <Card.Text>
-          Make the world a healthier place.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <br />
-    <Card className="text-center" border="light" bg='dark' text='white'>
-      <Card.Title>Background</Card.Title>
-    </Card>
-    <CardGroup>
-      <Card border="light" bg='dark' text='white'>
-        {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-        <Card.Body className="text-center">
-          <Card.Title>Lecturer</Card.Title>
-          <Card.Text>
-            I have taught over 7 unique university level courses.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card border="light" bg='dark' text='white'>
-        {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-        <Card.Body className="text-center">
-          <Card.Title>Researcher</Card.Title>
-          <Card.Text>
-            I am currently researching the Gut-Muscle-Brain-Axis, breath, and virtual reality as a mode for exercise.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card border="light" bg='dark' text='white'>
-        {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-        <Card.Body className="text-center">
-          <Card.Title>Strength and Conditioning Coach</Card.Title>
-          <Card.Text>
-            Currently the Head Strength Coach for a Division II NCAA program.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </CardGroup>
     <br />
     <Card className="text-center" border="light">
       <Card.Body>
-        <Card.Title>Web Development</Card.Title>
+        <Card.Title>Latest Research</Card.Title>
         <Card.Text>
-          In search of more scalable solutions to impact health outcomes, I have begun to learn web development.
-        </Card.Text>
-        <Button variant="dark">Learn More</Button>
-      </Card.Body>
-    </Card>
-    <br />
-    <Card className="text-center" border="light" bg='dark' text='white'>
-      <Card.Body>
-        <Card.Title>More About Me</Card.Title>
-        <Card.Text>
-          I love traveling, and seeing new places.  All of the pictures used here are experiences I have had the privilege to have.
+          Check out some of my latest research projects.
         </Card.Text>
       </Card.Body>
     </Card>
+    <CardColumns>
+      {/* first card */}
+      <Card>
+        <Card.Img variant="top" src={wser2019} alt="view of the race course for the W.S.E.R 2019" />
+        <Card.Body>
+          <Card.Title>Gut-Muscle-Axis 2019</Card.Title>
+          <Card.Text>
+          <a href="https://pubmed.ncbi.nlm.nih.gov/29989465/"><p class="card-text">Rapid gut microbiome changes in a world-class ultramarathon runner by Grosicki, Durk, & Bagley (2019).</p></a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      {/* second card */}
+      <Card>
+        <Card.Img variant="top" src={vo2max} alt="graded exercise test" />
+        <Card.Body>
+          <Card.Title>Gut-Muscle-Axis 2018</Card.Title>
+          <Card.Text>
+          <a href="https://pubmed.ncbi.nlm.nih.gov/29989465/"><p class="card-text">Gut microbiota composition is related to cardiorespiratoy fitness in healthy young adults by Durk and colleagues (2018).</p></a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      {/* third card */}
+      <Card>
+        <Card.Img variant="top" src={thaiIslands} alt="picture of the thai islands" className="text-center" style={{ height: '50rem', width: '38rem'}}  />
+        <Card.Body>
+          <Card.Title>More research is coming!</Card.Title>
+          <Card.Text>
+            <p>Science doesn't happen alone, it is a team endeavour.  Check out some of the researchers I have worked with!</p>
+              <a href="https://www.musclephyslab.com/"><p>Muscle Physiology Lab</p></a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+</CardColumns>
     </>
     );
   };
